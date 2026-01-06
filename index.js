f6ead7b18cffc72d931fce6c9f269f3c1ccf8447
chrismanifold-jpg <chrismanifold@gmail.com>
2026-01-07 00:19:31 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 import express from "express";
-import fetch from "node-fetch";
 
 const app = express();
-app.use(express.json());
-
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
