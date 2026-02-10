0b99168b867f6dc884722887767386825a0d700e
chrismanifold-jpg <chrismanifold@gmail.com>
2026-02-10 22:08:36 +0800

    Update googleDrive.js

--- googleDrive.js
+++ googleDrive.js
@@ -1,1 +1,17 @@
+import { google } from "googleapis";
+
+/**
+ * Creates an authenticated Google Drive client
+ */
+export const getDriveClient = () => {
+  const auth = new google.auth.GoogleAuth({
+    credentials: {
+      client_email: process.env.GOOGLE_CLIENT_EMAIL,
+      private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    },
+    scopes: ["https://www.googleapis.com/auth/drive.readonly"],
+  });
+
+  return google.drive({ version: "v3", auth });
+};
 
