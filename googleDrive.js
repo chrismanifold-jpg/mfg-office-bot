8c815974c6a82ed81a9fb23c72b58fe9a7be7a5d
chrismanifold-jpg <chrismanifold@gmail.com>
2026-02-10 23:26:32 +0800

    Update googleDrive.js

--- googleDrive.js
+++ googleDrive.js
@@ -1,19 +1,20 @@
 import { google } from "googleapis";
 
-const auth = new google.auth.GoogleAuth({
-  credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON),
-  scopes: ["https://www.googleapis.com/auth/drive.readonly"]
-});
+/**
+ * Lists SOP files from Google Drive
+ */
+export async function listSOPFiles() {
+  const auth = new google.auth.GoogleAuth({
+    credentials: JSON.parse(process.env.GOOGLE_SERVICE_ACCOUNT_JSON),
+    scopes: ["https://www.googleapis.com/auth/drive.readonly"]
+  });
 
-const drive = google.drive({ version: "v3", auth });
-
-export const listSOPFiles = async () => {
-  const folderId = process.env.GOOGLE_DRIVE_SOP_FOLDER_ID;
+  const drive = google.drive({ version: "v3", auth });
 
   const res = await drive.files.list({
-    q: `'${folderId}' in parents and trashed = false`,
-    fields: "files(id, name, mimeType)",
+    q: "mimeType != 'application/vnd.google-apps.folder'",
+    fields: "files(id, name, mimeType)"
   });
 
   return res.data.files || [];
-};
+}
